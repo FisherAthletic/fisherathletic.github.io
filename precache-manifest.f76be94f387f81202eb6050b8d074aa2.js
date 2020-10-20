c9efa95f75e20b6eeb2c624bfb63cd96712e6c1e
Yobo <yobany.perez@fisherathletic.com>
2020-10-20 10:05:22 -0400

    Updates

---
+++ precache-manifest.f76be94f387f81202eb6050b8d074aa2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "940815b210f64d65b0e4cf1f9caaf47e",
+    "url": "/website-reactjs/index.html"
+  },
+  {
+    "revision": "3e2a725963ede9377c64",
+    "url": "/website-reactjs/static/css/2.37dff9d5.chunk.css"
+  },
+  {
+    "revision": "d3ef419234870994c584",
+    "url": "/website-reactjs/static/css/main.951ca77f.chunk.css"
+  },
+  {
+    "revision": "3e2a725963ede9377c64",
+    "url": "/website-reactjs/static/js/2.fe2d7c63.chunk.js"
+  },
+  {
+    "revision": "fbfb5e42a41e7004782949e93fdf296a",
+    "url": "/website-reactjs/static/js/2.fe2d7c63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3ef419234870994c584",
+    "url": "/website-reactjs/static/js/main.ca1d4bb0.chunk.js"
+  },
+  {
+    "revision": "ffaf5d836472736590c5",
+    "url": "/website-reactjs/static/js/runtime-main.aea72e68.js"
+  },
+  {
+    "revision": "ac77a555ab3b726e75188d8409a3e540",
+    "url": "/website-reactjs/static/media/fisher-logo-01.ac77a555.svg"
+  }
+]);
